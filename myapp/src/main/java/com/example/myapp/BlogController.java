9636d6fc5fef5f74e564e6c8cf5e993e21f751b4
Lidiane Taquehara <lidi.mayra@gmail.com>
2025-01-10 00:14:37 +0000

    Setup a blog controller containing the seven RESTful routes:
    - index
    - new
    - create
    - show
    - edit
    - update
    - destroy

---
+++ myapp/src/main/java/com/example/myapp/BlogController.java
@@ -0,0 +1,48 @@
+package com.example.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/posts")
+    public String listPosts() {
+        return "blog/index";
+    }
+
+    @GetMapping("/posts/new")
+    public String newPost() {
+        return "blog/new";
+    }
+
+    @PostMapping("/posts")
+    public String createPost() {
+        //TODO: logic responsible for saving a post
+        return null;
+    }
+
+    @GetMapping("/posts/{postId}")
+    public String showPost() {
+        return "blog/show";
+    }
+
+    @GetMapping("/posts/{postId}/edit")
+    public String editPost() {
+        return "blog/edit";
+    }
+
+    @PatchMapping("/posts/{postId}")
+    public String updatePost() {
+        //TODO: logic responsible for updating a post
+        return null;
+    }
+
+    @DeleteMapping("/posts/{postId}")
+    public String deletePost() {
+        //TODO: logic responsible for deleting a post
+        return null;
+    }
+}
