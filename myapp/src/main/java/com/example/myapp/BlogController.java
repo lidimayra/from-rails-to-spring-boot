da63ee6f30bf46dd7cc5fa1904ee3d545ec2dead
Lidiane Taquehara <lidi.mayra@gmail.com>
2025-01-10 00:57:08 +0000

    Display posts list in /posts page
    Adjust BlogController and index.html to display a list of posts when
    accessing `/posts`

--- myapp/src/main/java/com/example/myapp/BlogController.java
+++ myapp/src/main/java/com/example/myapp/BlogController.java
@@ -2,10 +2,13 @@ package com.example.myapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
@@ -14,7 +17,9 @@ public class BlogController {
     private PostRepository postRepository;
 
     @GetMapping("/posts")
-    public String listPosts() {
+    public String listPosts(Model model) {
+        List<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog/index";
     }
 
